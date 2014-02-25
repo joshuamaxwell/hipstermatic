04105854d301bb6e90bca6f09fa4024a1d3cca6c
JMaxwell <maxwell.joshua@gmail.com>
2014-02-24 23:01:50 -0500

    initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
 console.log('\'Allo \'Allo!');
+
+var results = $.ajax({
+type: 'GET',
+url:'https://openapi.etsy.com/v2/listings/active.js?callback=etsyResults&api_key=kr9rjq7dc9c24jv6fccq2hus',
+data: ''
+})
